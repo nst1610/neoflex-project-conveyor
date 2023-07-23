a05efeebfac68020d5f384079dff07613dbdf2e1
Анастасия Семенова <nastyasem65@gmail.com>
2023-07-23 23:51:32 +0300

    Initial commit

---
+++ src/main/java/com/github/nst1610/neoflex/project/conveyor/ConveyorApplication.java
@@ -0,0 +1,13 @@
+package com.github.nst1610.neoflex.project.conveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveyorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveyorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nst1610/neoflex/project/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nst1610.neoflex.project.conveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
